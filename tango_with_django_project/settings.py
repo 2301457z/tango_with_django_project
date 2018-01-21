d7064cfec15ef97ec36895e454c047fed335a5f4
Jianwen Zhou <2301457z@student.gla.ac.uk>
2018-01-21 16:38:06 +0000

    Chapter 3 finished

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -67,6 +68,8 @@ TEMPLATES = [
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
