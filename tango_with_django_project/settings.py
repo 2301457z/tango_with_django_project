33f11c645d4950c698cd81708af955cf5a6fae3b
Jianwen Zhou <2301457z@student.gla.ac.uk>
2018-01-23 17:19:42 +0000

    new Chapter4

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -120,8 +121,9 @@ USE_L10N = True
 USE_TZ = True
 
 # Media files
-MEDIA_URL = '/media/'
 MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
 
 
 # Static files (CSS, JavaScript, Images)
